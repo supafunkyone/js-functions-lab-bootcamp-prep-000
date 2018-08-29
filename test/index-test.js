2271cb7d4d3944369e98732a3953071f3ea90cc0
alexsoleleris@gmail.com <alexsoleleris@gmail.com>
2018-08-29 21:16:56 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -15,7 +15,7 @@ describe('functions', () => {
 
   describe('happyHolidayTo(holiday, name)', () => {
     it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
+      expect(happyHolidayTo('Independence Day', 'Alex')).toEqual('Happy Independence Day, Alex!')
     })
   })
 
