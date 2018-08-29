063b20800af3a78fe55de1021baf7db23934a7ee
alexsoleleris@gmail.com <alexsoleleris@gmail.com>
2018-08-29 20:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(Alex) {
-  return "Happy Holiday, Alex!"
+function happyHolidaysTo(string) {
+  return "Happy Holidays, Alex!"
 }
